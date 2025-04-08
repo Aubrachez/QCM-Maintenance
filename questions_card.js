38f6f414c072c9517c838d2c1124eb6c09dd1e90
Aubrachez <145553794+Aubrachez@users.noreply.github.com>
2025-04-08 12:04:25 +0200

    Add files via upload

---
+++ questions_card.js
@@ -0,0 +1,6 @@
+const quizQuestions = [
+    { "Quelle est la capitale de la France ?": "Paris" ,"acquis":0},
+    { "Combien de continents existe-t-il ?": "7" ,"acquis":0},
+    { "Quel est le symbole chimique de l'eau ?": "H2O","acquis":0 }
+  ];
+  
